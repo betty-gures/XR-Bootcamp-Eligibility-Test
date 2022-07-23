9911759b5c2fe5b7617219db4de2e1c0e1995188
Fatma Betül Güreş <60520606+betty-gures@users.noreply.github.com>
2022-07-23 19:27:43 +0300

    #3 Soved
    By adding a boolean, no piece can be added if the spot is alredy marked.

--- Assets/Scripts/ClickTrigger.cs
+++ Assets/Scripts/ClickTrigger.cs
@@ -13,7 +13,8 @@ public class ClickTrigger : MonoBehaviour
 
 	[SerializeField]
 	private bool canClick;
-
+	//we add one more boolean to see if the tic-tac-toe field is already marked. Needs to be public to be accessed by TicTacTocAI script
+	public bool isMarked;
 	private void Awake()
 	{
 		_ai = FindObjectOfType<TicTacToeAI>();
@@ -24,6 +25,8 @@ public class ClickTrigger : MonoBehaviour
 		_ai.onGameStarted.AddListener(AddReference);
 		_ai.onGameStarted.AddListener(() => SetInputEndabled(true));
 		_ai.onPlayerWin.AddListener((win) => SetInputEndabled(false));
+		//similar to player conditions, we need ai conditions as well
+		
 	}
 
 	private void SetInputEndabled(bool val){
@@ -33,13 +36,25 @@ public class ClickTrigger : MonoBehaviour
 	private void AddReference()
 	{
 		_ai.RegisterTransform(_myCoordX, _myCoordY, this);
-		canClick = true;
+		canClick = true;		
+		isMarked = false;
+
 	}
 
 	private void OnMouseDown()
 	{
 		if(canClick){
 			_ai.PlayerSelects(_myCoordX, _myCoordY);
+			canClick = false;
+			isMarked = true;
+
 		}
+	}
+
+	public void AIClick()
+	{
+		_ai.AiSelects(_myCoordX, _myCoordY);
+		canClick = false;
+		isMarked = true;
 	}
 }
