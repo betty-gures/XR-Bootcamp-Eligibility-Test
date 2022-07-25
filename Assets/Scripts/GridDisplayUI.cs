06f0ba359c62af3bc27e2970079e6e73f831c659
Fatma Betül Güreş <60520606+betty-gures@users.noreply.github.com>
2022-07-25 15:40:32 +0300

    Preparation Grid UI System

---
+++ Assets/Scripts/GridDisplayUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using TMPro;
+
+public class GridDisplayUI : MonoBehaviour
+{
+    public GridItem[,] gridItems;
+    public List<GridItem> gridItemsToRegisterOnStart;
+
+    private void Awake()
+    {
+        gridItems = new GridItem[3, 3];
+
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < gridItemsToRegisterOnStart.Count; i++)
+        {
+            gridItemsToRegisterOnStart[i].AddReferenceToGrid();
+        }
+    }
+
+    public void RegisterGridItem(int x, int y, GridItem gridItem)
+    {
+        gridItems[x, y] = gridItem;
+    }
+
+    public void UpdateGrid(int x, int y, TicTacToeState state)
+    {
+        TextMeshProUGUI textMesh = gridItems[x,y].GetComponent<TextMeshProUGUI>();
+        switch (state)
+        {
+            case TicTacToeState.circle:
+                textMesh.text = "[O]";
+                break;
+            case TicTacToeState.cross:
+                textMesh.text = "[X]";
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/GridItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridItem : MonoBehaviour
+{
+    [SerializeField] private int _xCoord;
+    [SerializeField] private int _yCoord;
+
+    public int xCoord { get => _xCoord; }
+    public int yCoord { get => _yCoord; }
+    GridDisplayUI grid;
+
+    private void Awake()
+    {
+       grid = FindObjectOfType<GridDisplayUI>();
+    }
+
+    public void AddReferenceToGrid()
+    {
+        grid.RegisterGridItem(_xCoord, _yCoord, this);
+    }
+}
