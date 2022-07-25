9bdec80b3b0077b5dfd422aacf7b9868f6fc7f6f
Fatma Betül Güreş <60520606+betty-gures@users.noreply.github.com>
2022-07-25 15:50:37 +0300

    Tic Tac Toe AI logic

--- Assets/Scripts/ClickTrigger.cs
+++ Assets/Scripts/ClickTrigger.cs
@@ -4,59 +4,58 @@ using UnityEngine;
 
 public class ClickTrigger : MonoBehaviour
 {
-	TicTacToeAI _ai;
+    TicTacToeAI _ai;
 
-	[SerializeField]
-	private int _myCoordX = 0;
-	[SerializeField]
-	private int _myCoordY = 0;
+    [SerializeField]
+    private int _myCoordX = 0;
+    [SerializeField]
+    private int _myCoordY = 0;
 
-	[SerializeField]
-	private bool canClick;
-	//we add one more boolean to see if the tic-tac-toe field is already marked. Needs to be public to be accessed by TicTacTocAI script
-	public bool isMarked;
-	private void Awake()
-	{
-		_ai = FindObjectOfType<TicTacToeAI>();
-	}
+    [SerializeField]
+    private bool canClick;
+    public bool IsClicked;
 
-	private void Start(){
+    private void Awake()
+    {
+        _ai = FindObjectOfType<TicTacToeAI>();
+    }
 
-		_ai.onGameStarted.AddListener(AddReference);
-		_ai.onGameStarted.AddListener(() => SetInputEndabled(true));
-		_ai.onPlayerWin.AddListener((win) => SetInputEndabled(false));
-		//similar to player conditions, we need ai conditions as well
-		_ai.onAIStartsMove += () => SetInputEndabled(false);
-		_ai.onAIFinishedMove += () => SetInputEndabled(true);
-		
-	}
+    private void Start()
+    {
 
-	private void SetInputEndabled(bool val){
-		canClick = val;
-	}
+        _ai.onGameStarted.AddListener(AddReference);
+        _ai.onGameStarted.AddListener(() => SetInputEnabled(true));
+        _ai.onPlayerWin.AddListener((win) => SetInputEnabled(false));
+        _ai.onAITurnStart += () => SetInputEnabled(false);
+        _ai.onAITurnEnd += () => SetInputEnabled(true);
+    }
 
-	private void AddReference()
-	{
-		_ai.RegisterTransform(_myCoordX, _myCoordY, this);
-		canClick = true;		
-		isMarked = false;
+    private void SetInputEnabled(bool val)
+    {
+        canClick = val;
+    }
 
-	}
+    private void AddReference()
+    {
+        _ai.RegisterTransform(_myCoordX, _myCoordY, this);
+        IsClicked = false;
+        canClick = true;
+    }
 
-	private void OnMouseDown()
-	{
-		if(canClick){
-			_ai.PlayerSelects(_myCoordX, _myCoordY);
-			canClick = false;
-			isMarked = true;
+    private void OnMouseDown()
+    {
+        if (canClick)
+        {
+            _ai.PlayerSelects(_myCoordX, _myCoordY);
+            IsClicked = true;
+            canClick = false;
+        }
+    }
 
-		}
-	}
-
-	public void AIClick()
-	{
-		_ai.AiSelects(_myCoordX, _myCoordY);
-		canClick = false;
-		isMarked = true;
-	}
+    public void AIClick()
+    {
+        _ai.AiSelects(_myCoordX, _myCoordY);
+        IsClicked = true;
+        canClick = false;
+    }
 }
--- Assets/Scripts/TicTacToeAI.cs
+++ Assets/Scripts/TicTacToeAI.cs
@@ -2,9 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 using UnityEngine.Events;
 
-public enum TicTacToeState{none, cross, circle}
+public enum TicTacToeState { none, cross, circle }
 
 [System.Serializable]
 public class WinnerEvent : UnityEvent<int>
@@ -14,75 +15,343 @@ public class WinnerEvent : UnityEvent<int>
 public class TicTacToeAI : MonoBehaviour
 {
 
-	int _aiLevel;
+    int _aiLevel;
 
-	TicTacToeState[,] boardState;
+    TicTacToeState[,] boardState;
 
-	[SerializeField]
-	private bool _isPlayerTurn;
+    [SerializeField]
+    private bool _isPlayerTurn;
 
-	[SerializeField]
-	private int _gridSize = 3;
-	
-	[SerializeField]
-	private TicTacToeState playerState = TicTacToeState.cross;
-	TicTacToeState aiState = TicTacToeState.circle;
+    [SerializeField]
+    private int _gridSize = 3;
 
-	[SerializeField]
-	private GameObject _xPrefab;
+    [SerializeField]
+    private TicTacToeState playerState = TicTacToeState.cross;
+    TicTacToeState aiState = TicTacToeState.circle;
 
-	[SerializeField]
-	private GameObject _oPrefab;
+    [SerializeField]
+    private GameObject _xPrefab;
 
-	public UnityEvent onGameStarted;
+    [SerializeField]
+    private GameObject _oPrefab;
 
-	public Action onAIStartsMove;
-	public Action onAIFinishedMove;
+    public UnityEvent onGameStarted;
+    public Action onAITurnStart;
+    public Action onAITurnEnd;
 
-	//Call This event with the player number to denote the winner
-	public WinnerEvent onPlayerWin;
+    //Call This event with the player number to denote the winner
+    public WinnerEvent onPlayerWin;
 
-	ClickTrigger[,] _triggers;
-	
-	private void Awake()
-	{
-		if(onPlayerWin == null){
-			onPlayerWin = new WinnerEvent();
-		}
-	}
+    ClickTrigger[,] _triggers;
+    GridDisplayUI gridDisplayUI;
 
-	public void StartAI(int AILevel){
-		_aiLevel = AILevel;
-		StartGame();
-	}
+    private void Awake()
+    {
+        if (onPlayerWin == null)
+        {
+            onPlayerWin = new WinnerEvent();
+        }
+        gridDisplayUI = FindObjectOfType<GridDisplayUI>();
+    }
 
-	public void RegisterTransform(int myCoordX, int myCoordY, ClickTrigger clickTrigger)
-	{
-		_triggers[myCoordX, myCoordY] = clickTrigger;
-	}
+    //Called in UIButton OnClick Events in the Starting Panel
+    public void StartAI(int AILevel)
+    {
+        _aiLevel = AILevel;
+        StartGame();
+    }
 
-	private void StartGame()
-	{
-		_triggers = new ClickTrigger[3,3];
-		onGameStarted.Invoke();
-	}
+    public void RegisterTransform(int myCoordX, int myCoordY, ClickTrigger clickTrigger)
+    {
+        _triggers[myCoordX, myCoordY] = clickTrigger;
+    }
 
-	public void PlayerSelects(int coordX, int coordY){
+    private void StartGame()
+    {
+        _triggers = new ClickTrigger[3, 3];
+        boardState = new TicTacToeState[3, 3];
+        onGameStarted.Invoke();
+    }
 
-		SetVisual(coordX, coordY, playerState);
-	}
+    public void PlayerSelects(int coordX, int coordY)
+    {
+        SetVisual(coordX, coordY, playerState);
+    }
 
-	public void AiSelects(int coordX, int coordY){
+    public void AiSelects(int coordX, int coordY)
+    {
+        SetVisual(coordX, coordY, aiState);
+    }
 
-		SetVisual(coordX, coordY, aiState);
-	}
+    private void SetVisual(int coordX, int coordY, TicTacToeState targetState)
+    {
+        Instantiate(
+            targetState == TicTacToeState.circle ? _oPrefab : _xPrefab,
+            _triggers[coordX, coordY].transform.position,
+            Quaternion.identity
+        );
+        UpdateBoardState(coordX, coordY, targetState);
+    }
 
-	private void SetVisual(int coordX, int coordY, TicTacToeState targetState)
-	{
-		Instantiate(
-			targetState == TicTacToeState.circle ? _oPrefab : _xPrefab,
-			_triggers[coordX, coordY].transform.position,
-			Quaternion.identity
-		);
-	}
+    IEnumerator StartAITurn()
+    {
+        onAITurnStart?.Invoke();
+        yield return new WaitForSeconds(0.5f);
+        _isPlayerTurn = false;
+        ClickTrigger clickTrigger = CalculateBestMove();
+        if (clickTrigger)
+        {
+            clickTrigger.AIClick();
+        }
+        else // This only works because the player always goes first
+        {
+            onPlayerWin.Invoke(-1);
+        }
+        yield return new WaitForSeconds(0.5f);
+        onAITurnEnd?.Invoke();
+    }
+
+    private void UpdateBoardState(int coordX, int coordY, TicTacToeState state)
+    {
+        boardState[coordX, coordY] = state;
+        gridDisplayUI.UpdateGrid(coordX, coordY, state);
+        if (CheckIfPlayerWon(state))
+        {
+            if (state == TicTacToeState.cross)
+            {
+                onPlayerWin.Invoke(0);
+            }
+            else if (state == TicTacToeState.circle)
+            {
+                onPlayerWin.Invoke(1);
+            }
+            return;
+        }
+        if (_isPlayerTurn)
+        {
+            StartCoroutine(StartAITurn());
+        }
+        else
+        {
+            _isPlayerTurn = true;
+        }
+    }
+
+    private ClickTrigger CalculateBestMove()
+    {
+        ClickTrigger diaganolPlay = CheckForDiaganolWin();
+        ClickTrigger horizontalPlay = CheckForHorizontalWin();
+        ClickTrigger verticalPlay = CheckForVerticalWin();
+
+        if (diaganolPlay != null)
+        {
+            return diaganolPlay;
+        }
+
+        if (horizontalPlay != null)
+        {
+            return horizontalPlay;
+        }
+
+        if (verticalPlay != null)
+        {
+            return verticalPlay;
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (boardState[i, j] == TicTacToeState.none)
+                {
+                    return _triggers[i, j];
+                }
+
+            }
+        }
+        //Couldnt find anywhere to play
+        return null;
+
+
+    }
+    private bool CheckIfPlayerWon(TicTacToeState state)
+    {
+        int[] mySquaresHorizontally = new int[3]; // How many squares are filled with enemy icons per grid.
+        int[] mySquaresVertically = new int[3]; // How many squares are filled with enemy icons per grid.
+        int mydiaganolSquaresRight = 0;
+        int mydiaganolSquaresLeft = 0;
+        //Check each grid horizontally and gather information
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (boardState[i, j] == state)
+                {
+                    mySquaresHorizontally[i]++;
+                }
+                if (boardState[j, i] == state)
+                {
+                    mySquaresVertically[i]++;
+                }
+
+                if (boardState[j, i] == state || boardState[i, j] == state)
+                {
+                    if ((j == 1 && i == 1) ||
+                    (i == 0 && j == 0) ||
+                    (j == 2 && i == 2))
+                    {
+                        mydiaganolSquaresRight++;
+                    }
+                    else if ((j == 1 && i == 1) ||
+                        (i == 0 && j == 2) ||
+                        (j == 2 && i == 0))
+                    {
+                        mydiaganolSquaresLeft++;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if (mySquaresHorizontally[i] == 3 || mySquaresVertically[i] == 3 || mydiaganolSquaresLeft == 3 || mydiaganolSquaresRight == 3)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private ClickTrigger CheckForHorizontalWin()
+    {
+        int[] enemySquaresNumber = new int[3]; // How many squares are filled with enemy icons per grid.
+
+        //Check each grid horizontally and gather information
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (boardState[i, j] == playerState)
+                {
+                    enemySquaresNumber[i]++;
+                }
+            }
+        }
+
+        //Check to make sure no rows have a win condition HORIZONTALLY attached and if they do play a block there
+        for (int i = 0; i < 3; i++)
+        {
+            if (enemySquaresNumber[i] == 2)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (boardState[i, j] == TicTacToeState.none)
+                    {
+                        return _triggers[i, j];
+                    }
+                }
+            }
+        }
+        //If nothing is found that we need to address return null
+        return null;
+    }
+
+    private ClickTrigger CheckForVerticalWin()
+    {
+        int[] enemySquaresNumber = new int[3]; // How many squares are filled with enemy icons per grid.
+
+        //Check each grid horizontally and gather information
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (boardState[j, i] == playerState)
+                {
+                    enemySquaresNumber[i]++;
+                }
+            }
+        }
+
+        //Check to make sure no rows have a win condition Vertically attached and if they do play a block there
+        for (int i = 0; i < 3; i++)
+        {
+            if (enemySquaresNumber[i] == 2)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (boardState[j, i] == TicTacToeState.none)
+                    {
+                        return _triggers[j, i];
+                    }
+                }
+            }
+        }
+        //If nothing is found that we need to address return null
+        return null;
+    }
+
+    private ClickTrigger CheckForDiaganolWin()
+    {
+        int enemyDiaganolSquaresRight = 0;
+        int enemyDiaganolSquaresLeft = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (boardState[j, i] == playerState || boardState[i, j] == playerState)
+                {
+                    if(i == 1 && j == 1)
+                    {
+                        enemyDiaganolSquaresRight++;
+                        enemyDiaganolSquaresLeft++;
+                    }
+                    
+                    if ((i == 0 && j == 0) ||
+                    (j == 2 && i == 2))
+                    {
+                        enemyDiaganolSquaresRight++;
+                    }
+                    
+                    if ((i == 0 && j == 2) ||
+                        (j == 2 && i == 0))
+                    {
+                        enemyDiaganolSquaresLeft++;
+                    }
+                }
+            }
+        }
+
+        if (enemyDiaganolSquaresLeft == 2)
+        {
+            if (boardState[0,2] == TicTacToeState.none)
+            {
+                return _triggers[0, 2];
+            }
+
+            if (boardState[1, 1] == TicTacToeState.none)
+            {
+                return _triggers[1, 1];
+            }
+            if (boardState[2, 0] == TicTacToeState.none)
+            {
+                return _triggers[2, 0];
+            }
+        }
+
+        if (enemyDiaganolSquaresRight == 2)
+        {
+            if (boardState[0, 0] == TicTacToeState.none)
+            {
+                return _triggers[0, 0];
+            }
+
+            if (boardState[1, 1] == TicTacToeState.none)
+            {
+                return _triggers[1, 1];
+            }
+            if (boardState[2, 2] == TicTacToeState.none)
+            {
+                return _triggers[2, 2];
+            }
+        }
+        return null;
+    }
 }
